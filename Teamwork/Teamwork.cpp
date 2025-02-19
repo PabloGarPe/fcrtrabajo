6c544e19417e9f1e9057fa4ef95fadcda4a8e91e
ferxades12 <ferxades12@gmail.com>
2025-02-19 18:48:25 +0100

    cpp code done

--- Teamwork/Teamwork.cpp
+++ Teamwork/Teamwork.cpp
@@ -2,8 +2,25 @@
 
 extern "C" bool IsValidAssembly(int a, int b, int c);
 
+//ID: 300028
+
 int main()
 {
 
 	return 0;
+}
+
+void ControlInAsm() {
+	using std::cout, std::cin;
+
+	int a, b, c;
+
+	cin >> a;
+	cin >> b;
+	cin >> c;
+
+	if (IsValidAssembly(a, b, c) == 0) {
+		cout << "Algo salió mal" << std::endl;
+		exit();
+	}
 }
