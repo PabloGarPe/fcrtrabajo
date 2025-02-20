082a57a9c002c855a494dc8ea92ad510f948f945
Pablo <pablogarciapernas@gmail.com>
2025-02-20 12:28:56 +0100

    Fixed with mask

--- Teamwork/Teamwork.cpp
+++ Teamwork/Teamwork.cpp
@@ -14,6 +14,8 @@ void MaskControl() {
 	unsigned int firstMask = 0x1; // 00000000 00000000 00000000 00000001
 	unsigned int secondMask = 0x100; // 00000000 00000000 00000001 00000000
 	unsigned int thirdMask = 0x8; // 00000000 00000000 00000000 00001000
+	unsigned int highMask = 0x0; // 00000000 00000000 00000000 00000000
+	unsigned int lowMask = 0xFFFFFFFF;
 
 
 	cout << "Ingresa el primer numero: ";
@@ -32,15 +34,15 @@ void MaskControl() {
 		//exit()
 	}
 
-	/*unsigned int highBits = a >> 32;
-	unsigned int lowBits = b & ((1 << 32) - 1);
-	unsigned int number = (highBits << 32) | lowBits;*/
-	//Se q el ejercicio pide desplazar x bits, pero en nuestro caso, es usar los 0 bits mas altos y el resto del otro numero
-	//luego es quedarnos con el segundo numero y hacer un desplazamiento de 32 bits en un unsigned int en C++ da problemas
-	unsigned int number = b; //esto es por nuestro enunciado
+	unsigned int highBits = a & highMask;
+	unsigned int lowBits = b & lowMask;
+	unsigned int number = highBits | lowBits;
+
 	if (number <= 0) {
 		cout << "Hubo algun fallo";
 		//exit
+	}
+}
 //ID: 300028
 
 void ControlInAsm() {
